7a13f5994560d2c87553ccf31ebd4c052859813d
bzp99 <bertalan.peter@bp99.eu>
2023-04-03 14:43:27 +0200

    Comply with a JS standard in generator script
    -> <https://standardjs.com/>

--- input_generator/bin/gen.js
+++ input_generator/bin/gen.js
@@ -1,71 +1,71 @@
-'use strict';
-
+import fs from 'fs'
+import yargs from 'yargs'
+import { Hex } from 'crypto-js/enc-hex'
+import { MerkleTree } from 'merkletreejs'
+import { SHA256 } from 'crypto-js/sha256'
 
 /****************************************************************
  *      HELPERS                                                 *
  ****************************************************************/
 
 /* A string representing zero. */
-const ZEROSTR = '0';
+const ZEROSTR = '0'
 
 /*
  * Split a string every 8 characters.
  */
-function split8(string) {
-	return string.match(/.{1,8}/g);
+function split8 (string) {
+  return string.match(/.{1,8}/g)
 }
 
 /*
  * Prefix all elements of a string array with '0x'.
  */
-function map_0xprefix(arr) {
-	return arr.map(s => '0x' + s);
+function prefixWith0x (arr) {
+  return arr.map(s => '0x' + s)
 }
-
 
 /*
  * Expand a simple number into a stringified u32[8] array for ZoKrates.
  */
-function expand_number(number) {
-	const revstr = s => Array.from(s).reverse().join('');
-	const parts = split8(revstr(number.toString(16)))
-		      .map(p => '0x' + revstr(p))
-		      .map(p => parseInt(p, 16).toString())
-		      .reverse();
+function expandNumber (number) {
+  const revstr = s => Array.from(s).reverse().join('')
+  const parts = split8(revstr(number.toString(16)))
+    .map(p => '0x' + revstr(p))
+    .map(p => parseInt(p, 16).toString())
+    .reverse()
 
-	return [...Array(8 - parts.length).fill(ZEROSTR), ...parts];
+  return [...Array(8 - parts.length).fill(ZEROSTR), ...parts]
 }
-
 
 /****************************************************************
  *      CONSTANTS                                               *
  ****************************************************************/
 
-const orig_dummy_address = '0000000000000000000000000000000000000000';
-const orig_addresses = [
-	'00d7b92aec4093dd1994262bf85d72350f616a9c',
-	'117b390975488a3b0b78767c9a498d6db296cad3',
-	'2253e0a69100d2e30dc1557b24c425d46e0563d6',
-	'33a5277b649b3ad11c90c224be7c542f8a6d9a23',
-	'445851125b058edc674fca2eb22668ec7e1d0fe9',
-	'5515a4c05a074c226fcab8775b8dc930eaf2741a',
-	'66b87890fa0a7dba21e573f5461f5872fdc4ad49',
-	'77293cc1e2cc2853c9b704a2cb411c00e9bc2b59',
-	'88f87e520305728ddb098f14ae32f137e8ced839',
-	'99aff8eeacb535966c8a6d546bf6bcb05604c47d',
-	'aa3bf5d2fe11f8d65a3e977dc713c3347604cbe5',
-	'bb106c7134c9e06c0b8a6771042b749cdf04c759',
-	'cc6831e3f77ed2a2125e8525ebf4fee1587deb32',
-	'dd73c9cb45e6c8ac50f5240404b48f2a575de768',
-	'ee870023dbd50e039bbdb709b6c5edbcc2b76142',
-	'ffd00efdb947a4c93d5ca4f2d5c916565a0baace',
-];
+const dummyAddressOriginal = '0000000000000000000000000000000000000000'
+const addressesOriginal = [
+  '00d7b92aec4093dd1994262bf85d72350f616a9c',
+  '117b390975488a3b0b78767c9a498d6db296cad3',
+  '2253e0a69100d2e30dc1557b24c425d46e0563d6',
+  '33a5277b649b3ad11c90c224be7c542f8a6d9a23',
+  '445851125b058edc674fca2eb22668ec7e1d0fe9',
+  '5515a4c05a074c226fcab8775b8dc930eaf2741a',
+  '66b87890fa0a7dba21e573f5461f5872fdc4ad49',
+  '77293cc1e2cc2853c9b704a2cb411c00e9bc2b59',
+  '88f87e520305728ddb098f14ae32f137e8ced839',
+  '99aff8eeacb535966c8a6d546bf6bcb05604c47d',
+  'aa3bf5d2fe11f8d65a3e977dc713c3347604cbe5',
+  'bb106c7134c9e06c0b8a6771042b749cdf04c759',
+  'cc6831e3f77ed2a2125e8525ebf4fee1587deb32',
+  'dd73c9cb45e6c8ac50f5240404b48f2a575de768',
+  'ee870023dbd50e039bbdb709b6c5edbcc2b76142',
+  'ffd00efdb947a4c93d5ca4f2d5c916565a0baace'
+]
 
-const split_address = a => map_0xprefix(split8(a));
+const splitAddress = a => prefixWith0x(split8(a))
 
-const dummy_address = split_address(orig_dummy_address);
-const addresses = orig_addresses.map(a => split_address(a));
-
+const dummyAddress = splitAddress(dummyAddressOriginal)
+const addresses = addressesOriginal.map(a => splitAddress(a))
 
 /****************************************************************
  *      MAIN FUNCTIONS                                          *
@@ -75,227 +75,220 @@ const addresses = orig_addresses.map(a => split_address(a));
  * Transform simple balances input `input_bals' to conform with the
  * balances audit program.
  */
-function transform_balances(input_bals, total_accs) {
-	let bals = Object.entries(input_bals).map(([acc, bal]) => [
-		addresses[acc], expand_number(bal),
-	]);
-	bals.push(...Array(total_accs - balances.length).fill([
-		dummy_address, expand_number(ZEROSTR),
-	]));
+function transformBalances (inputBalances, totalAccounts) {
+  const bals = Object.entries(inputBalances).map(([acc, bal]) => [
+    addresses[acc], expandNumber(bal)
+  ])
+  bals.push(...Array(totalAccounts - bals.length).fill([
+    dummyAddress, expandNumber(ZEROSTR)
+  ]))
 
-	return bals;
+  return bals
 }
 
 /*
  * Transform simple transactions input `input_txs' to conform with the
  * audit programs.
  */
-function transform_transactions(input_txs, total_txs) {
-	let txs = input_txs.map(t => ({
-		source:         addresses[t.source],
-		destination:    addresses[t.destination],
-		amount:         expand_number(t.amount),
-	}));
-	const n_txs = txs.length;
-	txs.push(...Array(total_txs - n_txs).fill({
-		source:         dummy_address,
-		destination:    dummy_address,
-		amount:         expand_number(ZEROSTR),
-	}));
+function transformTransactions (inputTransactions, totalTransactions) {
+  const transactions = inputTransactions.map(t => ({
+    source: addresses[t.source],
+    destination: addresses[t.destination],
+    amount: expandNumber(t.amount)
+  }))
+  const txCount = transactions.length
+  transactions.push(...Array(totalTransactions - txCount).fill({
+    source: dummyAddress,
+    destination: dummyAddress,
+    amount: expandNumber(ZEROSTR)
+  }))
 
-	return { transactions: txs, n_transactions: n_txs };
+  return { transactions, txCount }
 }
 
 /*
  * Transform simple blocks input `input_blks' to conform with the
  * balances and whitelist audit programs.
  */
-function transform_blocks(input_blks, total_blks, total_txs) {
-	let blks = input_blks.map(
-	    b => transform_transactions(b, total_txs)
-	);
-	const n_blks = blks.length;
-	blks.push(...Array(total_blks - n_blocks).fill({
-		transactions: Array(argv.m).fill({
-			source:         dummy_address,
-			destination:    dummy_address,
-			amount:         expand_number(ZEROSTR),
-		}),
-		n_transactions: ZEROSTR,
-	}));
+function transformBlocks (inputBlocks, totalBlocks, totalTransactions) {
+  const blocks = inputBlocks.map(
+    b => transformTransactions(b, totalTransactions)
+  )
+  const blockCount = blocks.length
+  blocks.push(...Array(totalBlocks - blockCount).fill({
+    transactions: Array(argv.m).fill({
+      source: dummyAddress,
+      destination: dummyAddress,
+      amount: expandNumber(ZEROSTR)
+    }),
+    n_transactions: ZEROSTR
+  }))
 
-	return { blocks: blks, n_blocks: n_blks.toString() };
+  return { blks: blocks, blockCount: blockCount.toString() }
 }
 
 /*
  * Transform simple whitelist input `input_wl' to conform with the
  * whitelist audit program.
  */
-function transform_whitelist(input_wl, total_entries) {
-	let wl = input_wl.map(a => addresses[a]);
-	const n_wl = wl.length;
-	wl.push(...Array(total_entries - n_wl).fill(dummy_address));
+function transformWhitelist (inputWhitelist, totalEntries) {
+  const wl = inputWhitelist.map(a => addresses[a])
+  const wlCount = wl.length
+  wl.push(...Array(totalEntries - wlCount).fill(dummyAddress))
 
-	return { whitelist: wl, n_whitelist: n_wl.toString() };
+  return { whitelist: wl, n_whitelist: wlCount.toString() }
 }
 
 /*
  * Calculate the Merkle-tree root for simple array of
  * transactions `input_txs'.
  */
-function calc_root(input_txs) {
-	const { MerkleTree } = require('merkletreejs');
-	const SHA256 = require('crypto-js/sha256');
-	const Hex = require('crypto-js/enc-hex');
+function calculateRoot (inputTransactions) {
+  const int2hex = x => {
+    const buf = Buffer.alloc(4)
+    buf.writeInt32BE(x)
+    return buf.toString('hex')
+  }
+  const remove0x = x => x.slice(2)
 
-	const int2hex = x => {
-		const buf = Buffer.alloc(4);
-		buf.writeInt32BE(x);
-		return buf.toString('hex');
-	}
-	const remove0x = x => x.slice(2);
+  const padTx = t => [
+    ...Array(8 + 6).fill('00000000'),
+    ...t.source.map(p => remove0x(p)),
+    ...t.destination.map(p => remove0x(p)),
+    ...t.amount.map(p => int2hex(p))
+  ].join('')
 
-	const padTx = t => [
-		...Array(8 + 6).fill('00000000'),
-		...t.source.map(p => remove0x(p)),
-		...t.destination.map(p => remove0x(p)),
-		...t.amount.map(p => int2hex(p)),
-	].join('');
+  const tree = new MerkleTree(
+    inputTransactions.map(t => SHA256(Hex.parse(padTx(t))))
+  )
+  const root = tree.getRoot().toString('hex')
 
-	const tree = new MerkleTree(
-	    input_txs.map(t => SHA256(Hex.parse(padTx(t))))
-	);
-	const root = tree.getRoot().toString('hex');
-
-	return map_0xprefix(split8(root));
+  return prefixWith0x(split8(root))
 }
 
 // ---------------------------------------------------------------------
-
 
 /****************************************************************
  *      MAIN SCRIPT                                             *
  ****************************************************************/
 
-const argv = require('yargs/yargs')(process.argv.slice(2))
-	     .usage("usage: $0 <options> input")
-	     .example("$0 -n 10 -m 10 -b 3 -w 10 -t balances txs.json",
-		      "Generate an input.json from txs.json with"
-		      + " 10 blocks, 10 transactions in each block,"
-		      + " 3 accounts in total, and a whitelist of"
-		      + " 10 entries for balances audit")
-	     .command({
-		command: "balances <input>",
-		describe: "Generate input for the balances auditor",
-		builder: {
-			n: {
-				describe: "Number of blocks",
-				demandOption: true,
-				type: 'number',
-			},
-			m: {
-				describe: "Number of transactions"
-					+ " per block",
-				demandOption: true,
-				type: 'number',
-			},
-			b: {
-				describe: "Number of accounts",
-				demandOption: true,
-				type: 'number',
-			},
-		},
-		handler: argv => argv.type = 'balances',
-	     })
-	     .command({
-		command: "whitelist <input>",
-		describe: "Generate input for the whitelist auditor",
-		builder: {
-			n: {
-				describe: "Number of blocks",
-				demandOption: true,
-				type: 'number',
-			},
-			m: {
-				describe: "Number of transactions"
-					+ " per block",
-				demandOption: true,
-				type: 'number',
-			},
-			w: {
-				describe: "Length of whitelist",
-				demandOption: true,
-				type: 'number',
-			},
-		handler: argv => argv.type = 'whitelist',
-		},
-	     })
-	     .command({
-		command: "merkle <input>",
-		describe: "Generate input for the Merkle-tree auditor",
-		builder: {
-			m: {
-				describe: "Number of transactions"
-					+ " per block",
-				demandOption: true,
-				type: 'number',
-			},
-			i: {
-				describe: "Index of block (from zero)"
-					+ " which should be audited",
-				demandOption: true,
-				type: 'number',
-			},
-		},
-		handler: argv => argv.type = 'merkle',
-	     })
-	     .help('h')
-	     .wrap(80)
-	     .version(false)
-	     .argv
+const argv = yargs(process.argv.slice(2))
+  .usage('usage: $0 <options> input')
+  .example('$0 -n 10 -m 10 -b 3 -w 10 -t balances txs.json',
+    'Generate an input.json from txs.json with' +
+    ' 10 blocks, 10 transactions in each block,' +
+    ' 3 accounts in total, and a whitelist of' +
+    ' 10 entries for balances audit')
+  .command({
+    command: 'balances <input>',
+    describe: 'Generate input for the balances auditor',
+    builder: {
+      n: {
+        describe: 'Number of blocks',
+        demandOption: true,
+        type: 'number'
+      },
+      m: {
+        describe: 'Number of transactions per block',
+        demandOption: true,
+        type: 'number'
+      },
+      b: {
+        describe: 'Number of accounts',
+        demandOption: true,
+        type: 'number'
+      }
+    },
+    handler: argv => { argv.type = 'balances' }
+  })
+  .command({
+    command: 'whitelist <input>',
+    describe: 'Generate input for the whitelist auditor',
+    builder: {
+      n: {
+        describe: 'Number of blocks',
+        demandOption: true,
+        type: 'number'
+      },
+      m: {
+        describe: 'Number of transactions per block',
+        demandOption: true,
+        type: 'number'
+      },
+      w: {
+        describe: 'Length of whitelist',
+        demandOption: true,
+        type: 'number'
+      }
+    },
+    handler: argv => { argv.type = 'whitelist' }
+  })
+  .command({
+    command: 'merkle <input>',
+    describe: 'Generate input for the Merkle-tree auditor',
+    builder: {
+      m: {
+        describe: 'Number of transactions per block',
+        demandOption: true,
+        type: 'number'
+      },
+      i: {
+        describe: 'Index of block (from zero) which should be audited',
+        demandOption: true,
+        type: 'number'
+      }
+    },
+    handler: argv => { argv.type = 'merkle' }
+  })
+  .help('h')
+  .wrap(72)
+  .version(false)
+  .argv
 
 /* Read input data from file or stdin */
-const fs = require('fs')
 const data = JSON.parse(
-    (argv._.length < 1 || argv.input === '-')
+  (argv._.length < 1 || argv.input === '-')
     ? fs.readFileSync('/dev/stdin')
     : fs.readFileSync(argv.input, 'utf8')
-);
+)
 
-
-let result = [];
+const result = []
 switch (argv.type) {
-case 'balances':
-	result.push(transform_balances(data.balances, argv.b));
+  case 'balances': {
+    result.push(transformBalances(data.balances, argv.b))
 
-	const { b_blocks, b_n_blocks }
-	    = transform_blocks(data.blocks, argv.n, argv.m);
-	result.push(b_blocks);
-	result.push(b_n_blocks);
+    const { blocks, blockCount } =
+      transformBlocks(data.blocks, argv.n, argv.m)
+    result.push(blocks)
+    result.push(blockCount)
 
-	break;
+    break
+  }
 
-case 'whitelist':
-	const { w_blocks, w_n_blocks }
-	    = transform_blocks(data.blocks, argv.n, argv.m);
-	result.push(w_blocks);
-	result.push(w_n_blocks);
+  case 'whitelist': {
+    const { blocks, blockCount } =
+      transformBlocks(data.blocks, argv.n, argv.m)
+    result.push(blocks)
+    result.push(blockCount)
 
-	const { whitelist, n_whitelist }
-	    = transform_whitelist(data.whitelist, argv.w);
-	result.push(whitelist);
-	result.push(n_whitelist);
+    const { whitelist, wlCount } =
+      transformWhitelist(data.whitelist, argv.w)
+    result.push(whitelist)
+    result.push(wlCount)
 
-	break;
+    break
+  }
 
-case 'merkle':
-	/* XXX All transactions are considered, even dummy ones */
-	const { transactions, _ }
-	    = transform_transactions(data.blocks[argv.i], argv.m);
-	result.push(transactions);
+  case 'merkle': {
+    /* XXX All transactions are considered, even dummy ones */
+    // eslint-disable-next-line no-unused-vars
+    const { transactions, txCount: _ } =
+        transformTransactions(data.blocks[argv.i], argv.m)
+    result.push(transactions)
 
-	result.push(calc_root(transactions));
+    result.push(calculateRoot(transactions))
 
-	break;
+    break
+  }
 }
-console.log(JSON.stringify(result, null, 2));
+console.log(JSON.stringify(result, null, 2))
