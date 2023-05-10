382a933e0d0210b9f4df5f344d424fd410092f38
bzp99 <bertalan.peter@bp99.eu>
2023-05-10 12:17:48 +0200

    Add mean time measurement calculation script

---
+++ measurements/calculate_mean_times/calculate_mean_times.py
@@ -0,0 +1,51 @@
+import sys
+import contextlib
+import itertools
+import typing
+
+import pandas as pd
+import plac
+
+
+programs = ['balances', 'whitelist', 'merkle']
+measurements = ['compilation', 'setup', 'witness', 'proof']
+
+
+def smart_open(filename: str,
+               mode: str = 'r') -> (typing.IO[str]
+                                    | contextlib.nullcontext[typing.TextIO]):
+    """
+    Open file but simply return stdin or stdout if name is a hyphen
+
+    :param filename: name of file to open
+    :param mode: mode to open file in
+    :returns: filehandle to file
+    """
+    return (open(filename, mode)
+            if filename != '-'
+            else contextlib.nullcontext(sys.stdout
+                                        if mode == 'w'
+                                        else sys.stdin))
+
+
+@plac.opt('file', "Path to CSV input file", str)
+def main(file: str = '-'):
+    with smart_open(file) as f:
+        original = pd.read_csv(f)
+
+    df = pd.DataFrame(index=programs)
+    for p, m in itertools.product(programs, measurements):
+        sel = original[original['program'] == p][m]
+        df.at[p, f'{m}_mean'] = sel.mean()
+        df.at[p, f'{m}_stderr'] = sel.sem()
+
+    df.reset_index(inplace=True, names='program')
+    print(df.to_csv(index=False))
+
+
+def entry_point():
+    plac.call(main)
+
+
+if __name__ == '__main__':
+    entry_point()
